17906835b6ce9cde0e124319a4f91660e41012fb
PipWhite <pipwhite1999@gmail.com>
2023-09-27 17:21:30 +0100

    Completed project- play game method added

--- milestone_5.py
+++ milestone_5.py
@@ -3,7 +3,7 @@ import random
 word_list = ['apple', 'orange', 'kiwi', 'peach', 'melon']
 
 class Hangman:
-
+    # initialise method
     def __init__(self, word_list, num_lives=5):
         self.word_list = word_list
         self.num_lives = num_lives
@@ -12,32 +12,26 @@ class Hangman:
         self.num_letters = len(set(self.target_word))
         self.list_of_guesses = []
         
-        
-        print(f'Target Word: {self.target_word}')
-        print(f'Unique letters: {self.num_letters}')
-        print(f'Lives: {self.num_lives}')
-
-        
         for i in range(len(self.target_word)):
             self.word_guessed.append('_')
-        print(self.word_guessed)
-
+        
+    # method that checks to see if the inputted letter matches any in the target word
     def check_guess(self, letter_guess):
         letter_guess = letter_guess.lower()
 
         if letter_guess in self.target_word:
-            print(f'Good guess! {letter_guess} is in the word')
+            print(f'Good guess! {letter_guess} is in the word \n')
             for i in range(len(self.target_word)):
                 if self.target_word[i] == letter_guess:
                     self.word_guessed[i] = letter_guess
             self.num_letters -= 1
         else:
             self.num_lives -= 1
-            print(f'Sorry, {letter_guess} is not in the word. Try again.')
+            print(f'Sorry, {letter_guess} is not in the word. Try again.\n')
             print(f'You have {self.num_lives} lives left')
 
             
-    
+    # method that checks that the inputted value is a single aplohabetical character
     def ask_for_input(self):
         while True:
             letter_guess = input('Enter a single letter: ')
@@ -50,8 +44,20 @@ class Hangman:
                 break
         self.check_guess(letter_guess)
                 
-        
+ # Method creates a game and assigns the word_list and number of lives
+def play_game(word_list):
+    num_lives = 5
+    game = Hangman(word_list, num_lives)
+    while True: 
+        print(''.join(game.word_guessed))
+        if game.num_lives == 0:
+            print('You Lost!')
+            break
+        elif game.num_letters > 0:
+            game.ask_for_input()
+        elif game.num_lives != 0 and game.num_letters <= 0:
+            print('Congratulations. You won the game!')
+            break
 
-my_game = Hangman(word_list)
-my_game.ask_for_input()
 
+play_game(word_list)
