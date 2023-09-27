c53bcf5336f0f890b045f0d1cd3c2ea6e86c2b51
PipWhite <pipwhite1999@gmail.com>
2023-09-27 12:32:22 +0100

    Added check_guess and ask_for_input methods

--- milestone_2.py
+++ milestone_2.py
@@ -1,15 +1,35 @@
 import random
 
+def check_guess(letter_guess):
+    letter_guess = letter_guess.lower()
+
+    if letter_guess in target_word:
+        print(f'Good guess! {letter_guess} is in the word')
+        
+    else:
+        print(f'Sorry, {letter_guess} is not in the word. Try again.')
+
+
+def ask_for_input():
+    while True:
+        letter_guess = input('Enter a single letter: ')
+        if len(letter_guess) == 1 and letter_guess.isalpha() == True:
+            break
+        else:
+            print('Invalid letter. Please, enter a single alphabetical character.')
+    check_guess(letter_guess)
+    
+    
+
+
+
+
 word_list = ['apple', 'orange', 'kiwi', 'peach', 'melon']
 print(word_list)
 
-choice = random.choice(word_list)
-word = choice
-print(word)
+word_choice = random.choice(word_list)
+target_word = word_choice
+print(target_word)
 
-guess = input('Enter a single letter: ')
+ask_for_input()
 
-if len(guess) == 1 and guess.isalpha() == True:
-    print('Good guess')
-else:
-    print('Oops! That is not a valid input')
